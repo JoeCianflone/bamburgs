4998086fb2e3816c3ca218667cc5e015e5bb38b9
Joe Cianflone <joe@cianflone.co>
2018-01-09 14:10:59 -0500

    adds message for Dropin that makes sense

--- .bin/dropin.js
+++ .bin/dropin.js
@@ -1,7 +1,8 @@
 #! /usr/bin/env node
 const shell = require("shelljs");
 const fs = require('fs-extra');
-const package = require(process.cwd() + '/node_modules/bamburgs/package.json');
+const package = require(process.cwd() + '/package.json');
+const util = require('util');
 
 if (process.argv[2] == undefined) {
    console.log("You need to specifiy a folder: `npm run dropin path/to/place/files`");
@@ -9,24 +10,28 @@ if (process.argv[2] == undefined) {
    return false;
 }
 
-let folderPath = process.argv[2];
-
-fs.copy('./node_modules/bamburgs/src/sass', folderPath, (err) => {
+fs.copy('./src/sass', process.argv[2], (err) => {
    if (err) {
       return console.log(err);
    }
 });
 
-fs.copy('./node_modules/bamburgs/postcss.config.js', `${folderPath}/bamburgs.postcss.config.js`, (err) => {
+fs.copy('./postcss.config.js', `${process.argv[2]}/bamburgs.postcss.config.js`, (err) => {
    if (err) {
       return console.log(err);
    }
 });
 
+let options = {
+   "config": package.config,
+   "browserslist": package.browserslist,
+   "dependencies": package.dependencies
+};
+console.log("**************************************************");
+console.log("**************************************************");
 console.log ('Files Dropped into your folder. BUT THIS WONT YET WORK!');
 console.log('You will need to copy the following values to your package.json file');
-console.log(`config: { ${package.config} },`);
-console.log(`dependencies: { ${package.dependencies} },`);
-console.log(`browserlist: { ${package.browserlist} },`);
-console.log(`FINAL STEP: COPY ${folderPath}/bamburgs.postcss.config.js to your ROOT FOLDER`);
-
+console.log(JSON.stringify(options, null, 4));
+console.log(`FINAL STEP: COPY ${process.argv[2]}/bamburgs.postcss.config.js to your ROOT FOLDER`);
+console.log("**************************************************");
+console.log("**************************************************");
